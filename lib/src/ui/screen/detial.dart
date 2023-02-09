8c15874bd214c78bca3e0d36037d0756b1d2f851
cholanuch <cholanuch309651@gmail.com>
2023-02-09 13:37:05 +0700

    Add ui

--- lib/src/ui/screen/detial.dart
+++ lib/src/ui/screen/detial.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import 'Homepage.dart';
 
 class Detialscreen extends StatefulWidget {
   const Detialscreen({super.key});
@@ -10,13 +10,16 @@ class Detialscreen extends StatefulWidget {
 }
 
 class _DetialscreenState extends State<Detialscreen> {
+  int showborder = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff400E32),
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.transparent,
         actions: [],
-        title: Text('Usd-Thb'),
+        title: Text('Usd/Thb'.toUpperCase()),
       ),
       body: _buildbody(context),
     );
@@ -25,26 +28,198 @@ class _DetialscreenState extends State<Detialscreen> {
   Widget _buildbody(BuildContext context) {
     return Container(
       child: Column(children: [
-        Row(),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.6,
-          width: MediaQuery.of(context).size.width,
-          color: Colors.amber,
+        SizedBox(
+          height: 15,
         ),
-        Text('detail assets'),
         Container(
-          padding: EdgeInsets.all(15),
-          color: Colors.black,
-          child: Column(children: [
-            Row(),
-            Row(),
+          width: MediaQuery.of(context).size.width * 0.75,
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Buttontime(
+              showborder: showborder,
+              numberindex: 1,
+              word: '10m',
+              onpress: () {
+                setState(() {
+                  showborder = 1;
+                });
+              },
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Buttontime(
+              showborder: showborder,
+              numberindex: 2,
+              word: '10m',
+              onpress: () {
+                setState(() {
+                  showborder = 2;
+                });
+              },
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Buttontime(
+              showborder: showborder,
+              numberindex: 3,
+              word: '10m',
+              onpress: () {
+                setState(() {
+                  showborder = 3;
+                });
+              },
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Buttontime(
+              showborder: showborder,
+              numberindex: 4,
+              word: '10m',
+              onpress: () {
+                setState(() {
+                  showborder = 4;
+                });
+              },
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Buttontime(
+              showborder: showborder,
+              numberindex: 5,
+              word: '10m',
+              onpress: () {
+                setState(() {
+                  showborder = 5;
+                });
+              },
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Buttontime(
+              showborder: showborder,
+              numberindex: 6,
+              word: '10m',
+              onpress: () {
+                setState(() {
+                  showborder = 6;
+                });
+              },
+            ),
           ]),
         ),
-        Row(
-          children: [
-            TextButton(onPressed: () {}, child: Text('button')),
-            TextButton(onPressed: () {}, child: Text('button'))
-          ],
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.50,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.grey,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.05,
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            children: [
+              Text(
+                'Instrument USD/THB',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Text(
+                'Spread 0.010',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ),
+        Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+                color: Color(0xffA7727D),
+                borderRadius: BorderRadius.circular(15)),
+            height: MediaQuery.of(context).size.height * 0.10,
+            child: Container(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Date 2021-03-31 06:35:37',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w700),
+                      ),
+                      Text('CloseOut Bid :1.345',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w700))
+                    ],
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Tradable :True',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700)),
+                      Text('CloseOut Ask :1.345',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w700))
+                    ],
+                  )
+                ],
+              ),
+            )),
+        SizedBox(
+          height: 25,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  decoration: BoxDecoration(
+                      color: Color(
+                        0xffEDDBC7,
+                      ),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Follow'.toUpperCase(),
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.w700),
+                      ))),
+              Expanded(child: Container()),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                decoration: BoxDecoration(
+                    color: Color(
+                      0xffEDDBC7,
+                    ),
+                    borderRadius: BorderRadius.circular(15)),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text('trade now'.toUpperCase(),
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.w700))),
+              )
+            ],
+          ),
         )
       ]),
     );
